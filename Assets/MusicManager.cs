02010562981569e4c17c8e657af5c02a919196ee
Awesome Mike <magicmicheal4@gmail.com>
2022-07-17 18:46:01 +0100

    update music and music manager

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,10 +1,11 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
     public static MusicManager instance;
-    public AudioSource[] MusicChannels;
+    public List<WaveSource> MusicChannels = new List<WaveSource>();
     public int startingIndex = 0;
     private void Awake()
     {
@@ -13,24 +14,37 @@ public class MusicManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayIndex(0);
+        PlayIndex(0, 0);
     }
 
-    public void PlayIndex(int index)
+    public void PlayIndex(int wave, int index)
     {
-        for (int i = 0; i < MusicChannels.Length; i++)
+        for (int j = 0; j < MusicChannels.Count; j++)
+        {
+            for (int o = 0; o < MusicChannels[j].ads.Length; o++)
+            {
+                MusicChannels[j].ads[o].volume = 0;
+            }
+        }
+        for (int i = 0; i < MusicChannels[wave].ads.Length; i++)
         {
             if (i == index)
             {
-                MusicChannels[index].DOFade(1, .35f);
+                MusicChannels[wave].ads[index].DOFade(1, .35f);
             }
-            else if (MusicChannels[i].volume > 0)
+            else if (MusicChannels[wave].ads[i].volume > 0)
             {
-                MusicChannels[i].DOFade(0, .35f);
+                MusicChannels[wave].ads[i].DOFade(0, .35f);
             }
             else
-                MusicChannels[i].volume = 0;
+                MusicChannels[wave].ads[i].volume = 0;
 
         }
+    }
+
+    [System.Serializable]
+    public class WaveSource
+    {
+        public AudioSource[] ads;
     }
 }
